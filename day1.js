c50aec7fb19e0c5dbee4c03ad029e177181e0705
di-co-der <ragahvdivyansh9@gmail.com>
2024-07-13 08:04:05 +0530

    day1 js

---
+++ day1.js
@@ -0,0 +1,25 @@
+var num = 16;
+console.log(num);
+
+let name = "Divyansh";
+console.log(name);
+
+const booli = true;
+console.log(booli);
+
+let var1 = 1;
+let var2 = "Raghav";
+let var3 = true;
+let var4 = {name: "Divyansh",
+            learning : "JS"
+            };
+let var5 = [1,2,"e", true];
+
+console.log(typeof(var1));
+console.log(typeof(var2));
+console.log(typeof(var3));
+console.log(typeof(var4));
+console.log(typeof(var5));
+
+console.log(var4);
+console.log(var5);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+var num = 16;
+// console.log(num);
+
+// let name = "Divyansh";
+// console.log(name);
+
+// const booli = true;
+// console.log(booli);
