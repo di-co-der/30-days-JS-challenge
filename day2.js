d710fdc5d3c0e050856b7880bdc7409e119e4b11
di-co-der <ragahvdivyansh9@gmail.com>
2024-07-14 22:39:26 +0530

    day 2

---
+++ day2.js
@@ -0,0 +1,56 @@
+// var a = 1;
+// var b = 1;
+// console.log("sum is", a + b)
+
+// var a = 1;
+// var b = 1;
+// console.log("sum is", a - b)
+
+// var a = 1;
+// var b = 1;
+// console.log("sum is", a * b)
+
+// var a = 1;
+// var b = 1;
+// console.log("sum is", a / b)
+
+// var a = 1;
+// var b = 1;
+// console.log("sum is", a % b)
+
+// var a = 1;
+// var b = 1;
+// console.log("sum is", a += b)
+
+// var a = 1;
+// var b = 1;
+// console.log("sum is", a -= b)
+
+// var a = 1;
+// var b = 1;
+// console.log("sum is", a > b)
+
+// var a = 1;
+// var b = 1;
+// console.log("sum is", a == b)
+
+// var a = 1;
+// var b = 1;
+// console.log("sum is", a === b)
+
+// var a = 1;
+// var b = 1;
+// console.log("sum is", a && b)
+
+
+// var a = 1;
+// var b = 1;
+// console.log("sum is", a || b)
+
+// var a = 1;
+// var b = 1;
+// console.log("sum is",  ! b)
+
+var a = 1
+a>0 ? console.log("+ve") : console.log("-ve")
+
