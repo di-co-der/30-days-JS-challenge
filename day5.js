7c30210c1880835e5b390604f5685fbf736aa7f4
di-co-der <ragahvdivyansh9@gmail.com>
2024-07-18 14:57:51 +0530

    day 5

---
+++ day5.js
@@ -0,0 +1,90 @@
+// function oddeve(num){
+//     if((isNaN(num))) return 'enter valid number';
+//     if(num%2==0) return ('even');
+//     else return('odd');
+// }
+
+// document.getElementById('button').addEventListener('click', function(){
+//     var input = document.getElementById('inputs').value;
+//     var parseinput = parseInt(input);
+//     var result = oddeve(parseinput);
+//     document.getElementById('result').innerText = result;
+//     console.log(result);
+//     console.log(input);
+// })
+
+// function square(num){
+// return(num * num);
+// }
+
+// document.getElementById('button').addEventListener('click', function(){
+//     var input = document.getElementById('inputs').value;
+//     var parseinput = parseInt(input);
+//     var result = square(parseinput);
+//     document.getElementById('result').innerText = result;
+//     console.log(result);
+//     console.log(input);
+// })
+
+// function max(a,b){
+//     if(a>b) console.log(a);
+//     console.log(b);
+// }
+
+// var a = 2;
+// var b = 3;
+// max(a,b);
+
+
+// function con(a,b){
+//     console.log(a + b);
+//     // console.log(b);
+// }
+
+// var a = 'string';
+// var b = 'join';
+// con(a,b);
+
+
+// const sum = (a,b) => {
+//     console.log(a + b);
+//     // console.log(b);
+// }
+
+// var a = 1;
+// var b = 5;
+// sum(a,b);
+
+
+// const check = (str, char) => {
+//    return str.includes(char); 
+// }
+
+// var str = 'string';
+// var char = 't';
+// console.log(check(str,char));
+
+
+
+// const repeat= (func, times) => {
+//     for(var i= 0; i < times; i++)
+//         func();
+// }
+
+// const sayhello = ()=> console.log('hello');
+
+// repeat(sayhello, 3);
+
+
+// function func1(i){return i+1} ;
+// function func2(result){return result+1} ;
+
+
+
+// const two =(func1, func2, vari)=>{
+//     const res=func1(vari);
+//     console.log(func2(res));
+// }
+
+
+// two(func1, func2, 3);
