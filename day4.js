88a784686f41f824e13678ce97343730462c00b7
di-co-der <ragahvdivyansh9@gmail.com>
2024-07-18 12:34:51 +0530

    day 4

---
+++ day4.js
@@ -0,0 +1,48 @@
+// for(var i = 1; i<=10; i++) console.log(i);
+
+// for(var i = 1; i <=10; i++){
+//     console.log('5 * ', i, 'is ', i*5);
+// }
+
+// var i = 1;
+// var sum = 0;
+// while(i<=10){
+//     sum+=i;
+//     i++;
+// }
+// console.log(sum);
+
+// var i = 10;
+// while(i>=1){
+//     console.log(i);
+//     i= i-1;
+// }
+
+// var i = 1;
+// do{
+//     alert(i);
+//     console.log(i);
+//     i++;
+// }
+// while(i<=5);
+// document.getElementById('test').innerHTML = i;
+
+// let num = prompt("enter number");
+// num = parseInt(num);
+// var fact = 1;
+// do{
+//     fact = fact * num;
+//     num--;
+// }
+// while(num);
+// console.log(fact);
+
+// var pattern = '';
+// for(var i = 1; i <= 5; i++){
+//     for(var j = 1; j <= i; j++){
+//         pattern += '*';
+//     }
+//     pattern += "<br>";
+// }
+// console.log(pattern);
+//         document.getElementById('test').innerHTML = (pattern);
